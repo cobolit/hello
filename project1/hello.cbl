7f0a69b2cdbd30531d647bca52d49b4b5ae2f879
cobolit <dougslavens@sbcglobal.net>
2017-11-10 19:27:33 -0800

    update

--- project1/hello.cbl
+++ project1/hello.cbl
@@ -1,7 +1,7 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID.  hello.
       * adding a comment to hello.cbl 
-      * add additional comment - plus more content - change 7
+      * add additional comment - plus more content - change
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
